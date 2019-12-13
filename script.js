93c4bca64ab78caec057c76ff27328d55179892b
Brandon Downhour <xtremeelement@gmail.com>
2019-12-13 13:02:27 -0500

    fixed ajax request and added sticky header

--- script.js
+++ script.js
@@ -5,16 +5,19 @@
 var appID = "8762ceb6";
 var appKey = "87390675c80ad08855757b2abc17feb2";
 var data;
-var queryURL = `https://api.edamam.com/search?q=chicken&app_id=${appID}&app_key=${appKey}`;
-fetch(queryURL, {
-  method: "GET"
-}).then(function(response) {
-  data = response.json();
-  console.log(response.json());
-});
+var searchTerm = "chicken";
+
+var queryURL = `https://api.edamam.com/search?q=${searchTerm}&app_id=${appID}&app_key=${appKey}`;
+fetch(queryURL, { method: "GET" })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(res) {
+    data = res;
+    console.log(data);
+  });
 
 // APIKey = AIzaSyAaRcgnx00VKEpGmrynTsPq4RnDQNBQU9M
-
 // var title = "garlic";
 // var queryURL = "https://www.themealdb.com/api/json/v1/1/list.php?c="+ title;
 // //how to access API data (make http request)
@@ -23,7 +26,6 @@ fetch(queryURL, {
 //   method: "GET"
 // }).then(function(response) {
 //   console.log(response);//comes back as an object, then we traverse through object to call info
-
 // });
 
 $(function() {
